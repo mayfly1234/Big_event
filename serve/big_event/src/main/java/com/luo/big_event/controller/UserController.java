7f4f5b7b3a64b96150891912f62f8a97cef23d67
mayfly_1234 <3403555751@qq.com>
2026-06-19 10:11:23 +0800

    获取用户信息，@JsonIgnore,开启驼峰命名

--- serve/big_event/src/main/java/com/luo/big_event/controller/UserController.java
+++ serve/big_event/src/main/java/com/luo/big_event/controller/UserController.java
@@ -8,9 +8,7 @@ import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -56,6 +54,15 @@ public class UserController {
                 return Result.error("密码错误");
             }
         }
+    }
+
+    @GetMapping("/userInfo")
+    public Result<User> userInfo(@RequestHeader(name="Authorization") String token) {
+    Map<String,Object>map= JwtUtil.parseToken(token);
+    String username = (String)map.get("username");
+
+    User u = userService.findByUserName(username);
+    return Result.success(u);
     }
 
 
